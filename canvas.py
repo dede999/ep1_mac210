5a56a280f09f2bf2b59c7025cdc2d8f8ee3062a2
bruno-lucas <bruno.lucas@usp.br>
2018-12-08 18:06:56 -0200

    Added interface file and buttons
    adicionei os botões pra resetar as curvas e colocar mais. Também criei um novo arquivo com as funções de interface.

--- canvas.py
+++ canvas.py
@@ -1,7 +1,8 @@
 import sys
 import pygame
-import numpy as np
+import interface
 from bezier import *
+
 
 args = len(sys.argv)
 if args == 1:
@@ -23,55 +24,65 @@ else:
 	deg = int(sys.argv[2])
 	cc = int(sys.argv[1])
 
-
 pygame.init()
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
-size = [800, 800]
-purple = (255, 0, 255)
+size = [900, 900]
+purple = (155, 100, 255)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("EP1 - MAC 0210")
 
 clock = pygame.time.Clock()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 done = False
-
 
 c = [black, red, green, blue, purple]
 c_vec = []
 
-for i in range(cc):
-	c_i = []
-	for d in range(deg + 1):
-		p = Point(np.random.randint(0,799), np.random.randint(0,799))
-		c_i.append(p)
-	curve = Bezier(c_i)
-	curve.color = red
-	c_vec.append(curve)
-
-
+interface.curves(cc, c_vec, deg)
 
 while not done:
 	clock.tick(10)
 	mousepos = (0, 0)
 	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			done = True
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			for bez in c_vec:
 				bez.color = red
 			mousepos = pygame.mouse.get_pos()
 			clique = Point(mousepos[0], mousepos[1])
 			closest(clique, c_vec)
-		if event.type == pygame.QUIT:
-			done = True
+			if event.button == 1:
+				if reset.collidepoint(event.pos):
+					interface.curves(cc, c_vec, deg)
+				if add.collidepoint(event.pos):
+					ci = []
+					for d in range(deg + 1):
+						p = Point(np.random.randint(0, 799), np.random.randint(0, 799))
+						ci.append(p)
+					curve = Bezier(ci)
+					curve.color = red
+					c_vec.append(curve)
+
 	screen.fill(white)
+	reset = pygame.Rect(800, 50, 100, 50)
+	add = pygame.Rect(800, 150, 100, 50)
+	pygame.draw.rect(screen, blue, reset)
+	pygame.draw.rect(screen, purple, add)
+	textR = myfont.render('RESET', False, (0, 0, 0))
+	textA = myfont.render('ADD', False, (0, 0, 0))
+	screen.blit(textR, (800, 50))
+	screen.blit(textA, (800, 150))
+
 	for c in c_vec:
-		# list_pts = []
 		for i in range(len(c.x_pts) - 1):
-			# list_pts.append([c.x_pts[i], c.y_pts[i]])
-			pygame.draw.line(screen, c.color, [int(c.x_pts[i]), int(c.y_pts[i])],
-			                 [int(c.x_pts[i + 1]), int(c.y_pts[i + 1])], 1)
+			pygame.draw.line(screen, c.color, [int(c.x_pts[i]), int(c.y_pts[i])], [int(c.x_pts[i + 1]), int(c.y_pts[i + 1])], 1)
+
 	pygame.display.flip()
 
 pygame.quit()
